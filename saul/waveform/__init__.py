3972c5566b8967122a9b5d034fb8dfc8057d67a7
Liam Toney <liam@liam.earth>
2023-09-03 15:44:47 -0800

    Set up structure for the Stream object

---
+++ saul/waveform/__init__.py
@@ -0,0 +1,1 @@
+from saul.waveform.stream import Stream
---
+++ saul/waveform/stream.py
@@ -0,0 +1,8 @@
+"""
+Contains the definition of the Stream class.
+"""
+
+
+class Stream:
+    def __init__(self):
+        raise NotImplementedError
