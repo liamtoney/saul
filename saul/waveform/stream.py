8f1a86607447427440e7cacd8d2e300e8e8f17ab
Liam Toney <liam@liam.earth>
2023-09-04 11:56:19 -0800

    Add `from_local()` method

--- saul/waveform/stream.py
+++ saul/waveform/stream.py
@@ -4,7 +4,7 @@ Contains the definition of the Stream class.
 
 import obspy
 from obspy import UTCDateTime
-from waveform_collection import gather_waveforms
+from waveform_collection import gather_waveforms, read_local
 
 
 class Stream(obspy.Stream):
@@ -20,7 +20,30 @@ class Stream(obspy.Stream):
             endtime=UTCDateTime(*endtime),  # Convert from tuple
             merge_fill_value=False,
             trim_fill_value=False,
-            remove_response=False,  # KEY
-            verbose=True,
+        )
+        return cls(traces=st.traces)
+
+    @classmethod
+    def from_local(
+        cls,
+        data_dir,
+        coord_file,
+        network,
+        station,
+        channel,
+        starttime,
+        endtime,
+        location='*',
+    ):
+        st = read_local(
+            data_dir=data_dir,
+            coord_file=coord_file,
+            network=network,
+            station=station,
+            location=location,
+            channel=channel,
+            starttime=UTCDateTime(*starttime),  # Convert from tuple
+            endtime=UTCDateTime(*endtime),  # Convert from tuple
+            merge=False,
         )
         return cls(traces=st.traces)
