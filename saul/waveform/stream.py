713c9059ef73cdaffcd96b23d6a9ed668c1690fe
Liam Toney <liam@liam.earth>
2023-09-04 01:28:56 -0800

    Drafted `__init__()` for `Stream` class

--- saul/waveform/stream.py
+++ saul/waveform/stream.py
@@ -2,7 +2,28 @@
 Contains the definition of the Stream class.
 """
 
+import obspy
+from obspy import UTCDateTime
+from waveform_collection import gather_waveforms
 
-class Stream:
-    def __init__(self):
-        raise NotImplementedError
+
+class Stream(obspy.Stream):
+    def __init__(self, traces=None, **kwargs):
+        if kwargs:
+            assert traces is None, 'Cannot provide `traces` if requesting waveforms!'
+            if 'loc' not in kwargs:
+                kwargs['loc'] = '*'  # Basically, we're making `loc` an optional arg.
+            st = gather_waveforms(
+                source='IRIS',
+                network=kwargs['net'],
+                station=kwargs['sta'],
+                location=kwargs['loc'],
+                channel=kwargs['cha'],
+                starttime=UTCDateTime(*kwargs['start']),  # Convert from tuple
+                endtime=UTCDateTime(*kwargs['end']),  # Convert from tuple
+                merge_fill_value=False,
+                trim_fill_value=False,
+                verbose=True,
+            )
+            traces = st.traces
+        super().__init__(traces=traces)
