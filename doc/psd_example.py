97d3b768641585759f1be1c968252f729a4aa11c
Liam Toney <liam@liam.earth>
2023-09-04 16:17:52 -0800

    Just save the dang thing

--- doc/psd_example.py
+++ doc/psd_example.py
@@ -4,11 +4,11 @@ st = Stream.from_iris('AK', 'HOM', 'BDF', (2023, 9, 1, 0, 5), (2023, 9, 1, 0, 15
 st.detrend().taper(0.05).remove_response()  # SAUL Stream objects behave like ObsPy's
 PSD(st, method='multitaper').plot(show_noise_models=True)
 
+# ---------------------------------------
 # Below code not to be shown in README.md
-if False:
-    from pathlib import Path
+# ---------------------------------------
+from pathlib import Path
 
-    import matplotlib.pyplot as plt
+from matplotlib.pyplot import gcf
 
-    output_file = Path(__file__).with_name('psd_example.png')
-    plt.gcf().savefig(output_file, dpi=300, bbox_inches='tight')
+gcf().savefig(Path(__file__).with_name('psd_example.png'), dpi=300, bbox_inches='tight')
