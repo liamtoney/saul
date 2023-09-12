ef00dd7883cb9efc7225b158553234ac25caa2d1
Liam Toney <liam@liam.earth>
2023-09-11 22:29:23 -0800

    Remove unneeded parts of Sphinx conf file

--- doc/conf.py
+++ doc/conf.py
@@ -12,11 +12,9 @@ copyright = f'{datetime.now().year}, {author}'
 
 extensions = []
 
-templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
