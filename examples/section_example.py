0acd24af0874d78c1e7f06102f2e49fceed1db0d
Liam Toney <ltoney@usgs.gov>
2023-11-22 16:08:42 -0800

    Add example script for section plotting

---
+++ examples/section_example.py
@@ -0,0 +1,8 @@
+from saul import Stream
+
+st = Stream.from_iris(
+    'UU', 'ZNPU,KNB,LCMT', 'HHN', (2023, 11, 14, 22, 38), (2023, 11, 14, 22, 40)
+)
+st.detrend('demean').taper(0.05).remove_response(output='DISP')
+st.taper(0.05).filter('highpass', freq=2)
+st.plot(src_lat=37.26842, src_lon=-112.93468, reftime=(2023, 11, 14, 22, 38, 42))
