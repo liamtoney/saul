92d974350eed378a4faabc3cb3d85eabc3c524be
Liam Toney <ltoney@usgs.gov>
2024-08-23 13:38:09 -0600

    Ignore SyntaxWarning arising from docstring formatting

--- saul/spectral/__init__.py
+++ saul/spectral/__init__.py
@@ -2,6 +2,12 @@
 Contains tools for estimating and plotting spectra.
 """
 
-from saul.spectral.helpers import get_ak_infra_noise
+import warnings
+
+with warnings.catch_warnings():
+    # Ignore "SyntaxWarning: invalid escape sequence '\ '" arising from the docstring
+    # formatting in `get_ak_infra_noise()`
+    warnings.simplefilter('ignore', category=SyntaxWarning)
+    from saul.spectral.helpers import get_ak_infra_noise
 from saul.spectral.psd import PSD
 from saul.spectral.spectrogram import Spectrogram
