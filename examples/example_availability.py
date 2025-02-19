f71c8f962427d399534e051ec9b85fbc5bb0d471
Liam Toney <liam@liam.earth>
2025-02-19 09:53:27 -0700

    Use longer timespan for example

--- examples/example_availability.py
+++ examples/example_availability.py
@@ -4,7 +4,7 @@ df = Stream.from_earthscope(
     'AK,AV',
     'HOM,RC01,BAEI',
     '?DF,BHZ',
-    (2023, 9, 1, 0, 5),
-    (2023, 9, 1, 0, 15),
+    (2024, 8, 1),
+    (2025, 2, 1),
     just_availability=True,
 )
