74dd4eba36ba156e55ee08966b7816e6a87d8821
Liam Toney <liam@liam.earth>
2025-02-19 23:55:12 -0700

    Faster example

--- examples/example_availability.py
+++ examples/example_availability.py
@@ -2,10 +2,10 @@ from saul import get_availability
 
 df = get_availability(
     'AK,AV',
-    'HOM,RC01,BAEI',
+    'HOM,RC01,KENI',
     '?DF,BHZ',
     (2024, 8, 1),
-    (2025, 2, 1),
+    (2024, 10, 1),
     print_timespans=True,
     plot=True,
 )
