d44ec8b5d49cba2fd1f9d67d961e729dffa51449
Liam Toney <liam@liam.earth>
2025-03-09 12:36:15 -0600

    Organize `conf.py` better

--- _doc/conf.py
+++ _doc/conf.py
@@ -1,4 +1,4 @@
-# -- Project information -----------------------------------------------------
+# Project information ------------------------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import sys
@@ -10,7 +10,7 @@ project = 'SAUL'
 author = 'Liam Toney'
 copyright = f'{datetime.now().year}, {author}'
 
-# -- General configuration ---------------------------------------------------
+# General configuration ----------------------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
@@ -20,7 +20,18 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
 ]
+
+# Options for HTML output --------------------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {'collapse_navigation': False}
+
+# Options for various extensions -------------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/#built-in-extensions
+
 napoleon_numpy_docstring = False  # We are using Google docstring style
+
 autodoc_mock_imports = [
     'esi_core',
     'lxml',
@@ -32,7 +43,10 @@ autodoc_mock_imports = [
     'scipy',
     'waveform_collection',
 ]
+autodoc_member_order = 'bysource'
+
 autoclass_content = 'init'
+
 todo_include_todos = True
 
 # These only need to cover the packages we reference from the docstrings
@@ -47,14 +61,3 @@ intersphinx_mapping = dict(
     waveform_collection=('https://uaf-waveform-collection.readthedocs.io/en/master/', None),
 )
 # fmt: on
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'sphinx_rtd_theme'
-html_theme_options = {'collapse_navigation': False}
-
-# -- Options for sphinx.ext.autodoc  -----------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#configuration
-
-autodoc_member_order = 'bysource'
