c03348539222dec92b1017a6283fd16a4d171a24
Liam Toney <liam@liam.earth>
2025-03-08 23:08:44 -0700

    Simplify example

--- examples/example_psd_smooth.py
+++ examples/example_psd_smooth.py
@@ -9,17 +9,7 @@ st = Stream.from_earthscope(
 st.detrend().taper(0.05).remove_sensitivity()
 
 # Figure 1 — No smoothing
-PSD(st).plot()
-fig1 = plt.gcf()
-fig1.axes[0].set_title('No smoothing')
-fig1.tight_layout()
-fig1.show()
+PSD(st).plot(db_lim=(20, 110))
 
 # Figure 2 — Konno–Ohmachi smoothing
-bandwidth = 40
-PSD(st).smooth(bandwidth).plot()
-fig2 = plt.gcf()
-fig2.axes[0].set_title(f'Smoothing, bandwidth = {bandwidth}')
-fig2.axes[0].set_ylim(fig1.axes[0].get_ylim())  # Easier comparison
-fig2.tight_layout()
-fig2.show()
+PSD(st).smooth(40).plot(db_lim=(20, 110))
